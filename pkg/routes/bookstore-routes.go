02214260fbd14a6da2788f96fe16abbf900d4f99
zakhaev43 <imransuet@gmail.com>
2023-11-18 10:36:54 +0100

    tested Rest API successfully exit

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/zakhaev43/ToDo-in-Golang/pkg/controllers"
 )
 
-var RegisterBookStoreRoutes = func(router *mux.router) {
+
+var RegisterBookStoreRoutes = func (router *mux.Router){
 
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
